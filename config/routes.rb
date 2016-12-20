eae80779ad474fb12bd1351d9362e9e3546589d1
Joonas Tiala <joonas.tiala@gmail.com>
2016-12-20 16:38:03 +0200

    Added API V1

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  resources :menu_items
-  resources :menus
-  resources :listings
-  resources :restaurants
-  resources :lists
+		namespace :api do
+			namespace :v1 do
+				resources :menu_items, only: [:index, :show]
+				resources :menus, only: [:index, :show]
+				resources :listings, only: [:index, :show]
+				resources :restaurants, only: [:index, :show]
+				resources :lists, only: [:index, :show]
+			end
+	end
 end
